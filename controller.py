e233211d42f1090b3b31754bafd93d5e25cd9ce7
kyleromero10 <91986144+kyleromero10@users.noreply.github.com>
2023-12-09 12:06:53 -0500

    got like 65% of it done, oh god that was awful

--- controller.py
+++ controller.py
@@ -7,7 +7,10 @@ from scipy.io import wavfile
 import librosa
 import numpy as np
 import os  # Add this line to import the 'os' module
+import subprocess
+import matplotlib.pyplot as plt
 from pydub import AudioSegment
+
 
 class AudioController:
     def __init__(self, root):
@@ -31,11 +34,28 @@ class AudioController:
 
     def load_audio_file(self, file_path):
         if not file_path.lower().endswith('.wav'):
-            audio = AudioSegment.from_file(file_path)
-            y = np.array(audio.get_array_of_samples())
-            return audio.frame_rate, y
+            ffmpeg_path = os.path.join(os.path.dirname(__file__), 'ffmpeg.exe')  # Adjust the executable name if needed
+            output_path = os.path.join(os.path.dirname(file_path), 'output.wav')  # Adjust the output path as needed
+
+            # Use ffmpeg to convert the input file to WAV and remove metadata
+            command = [ffmpeg_path, '-i', file_path, '-acodec', 'pcm_s16le', '-ar', '44100', '-ac', '1', output_path]
+            subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+            # Load the processed WAV file
+            audio = AudioSegment.from_file(output_path)
+            channels = audio.channels
+            if channels > 1:
+                # Convert to mono (single channel) if there are multiple channels
+                audio = audio.set_channels(1)
+            audio_data = np.array(audio.get_array_of_samples())
+
+            # Remove the temporary output file
+            os.remove(output_path)
+
+            return audio.frame_rate, audio_data
         else:
             return wavfile.read(file_path)
+
 
     def load_wav(self, file_path):
         if not file_path.lower().endswith('.wav'):
@@ -47,21 +67,17 @@ class AudioController:
             return wavfile.read(file_path)
 
     def calculate_rt60(self, audio_data, sample_rate):
-        # Convert audio data to floating-point format
-        audio_data_float = librosa.util.normalize(audio_data.astype(float))
+        # Compute autocorrelation
+        autocorr = np.correlate(audio_data, audio_data, mode='full')
 
-        # Estimate autocorrelation function
-        autocorr = librosa.autocorrelate(audio_data_float)
+        # Get frequency bands
+        low_freqs, mid_freqs, high_freqs = self.get_frequency_ranges(sample_rate)
 
-        # Define frequency bands for Low, Mid, and High
-        low_freqs = (20 <= sample_rate / (np.arange(len(autocorr)) + np.finfo(float).eps)) & (sample_rate / (np.arange(len(autocorr)) + np.finfo(float).eps) < 250)
-        mid_freqs = (250 <= sample_rate / (np.arange(len(autocorr)) + np.finfo(float).eps)) & (sample_rate / (np.arange(len(autocorr)) + np.finfo(float).eps) < 1000)
-        high_freqs = (1000 <= sample_rate / (np.arange(len(autocorr)) + np.finfo(float).eps)) & (sample_rate / (np.arange(len(autocorr)) + np.finfo(float).eps) <= 5000)
-
-        # Integrate autocorrelation within each frequency band
+        # Compute RT60 for each frequency range
         rt60_low = self.compute_rt60(autocorr[low_freqs], sample_rate)
         rt60_mid = self.compute_rt60(autocorr[mid_freqs], sample_rate)
         rt60_high = self.compute_rt60(autocorr[high_freqs], sample_rate)
+
 
         return rt60_low, rt60_mid, rt60_high
 
@@ -72,21 +88,100 @@ class AudioController:
         if power.ndim == 1:
             power_sum = np.sum(power)
             t_half = np.argmax(power > 0.5 * power_sum)
+            rt60 = 2 * (len(autocorr) - t_half) / sample_rate
         else:
             power_sum = np.sum(power, axis=1)
             t_half = np.argmax(power_sum > 0.5 * power_sum[-1])
+            rt60 = 2 * (len(autocorr) - t_half) / sample_rate
 
-        # Calculate RT60 based on the time index
-        rt60 = 2 * (power.shape[0] - t_half) / sample_rate
+        # Create an array with RT60 values for each time point
+        rt60_array = 2 * (len(autocorr) - np.arange(len(autocorr))) / sample_rate
 
-        return rt60
+        return rt60_array
+
+    def get_frequency_ranges(self, sample_rate):
+        # Define your frequency bands (adjust the values as needed)
+        low_freqs = np.arange(0, 1000)  # Adjust the upper limit as needed
+        mid_freqs = np.arange(1000, 5000)  # Adjust the upper limit as needed
+        high_freqs = np.arange(5000, sample_rate / 2)  # Adjust the lower and upper limits as needed
+
+        # Convert high_freqs to integers
+        high_freqs = high_freqs.astype(int)
+
+        return low_freqs, mid_freqs, high_freqs
 
     def plot_rt60(self):
-        rt60_low = self.model.rt60_low
-        rt60_mid = self.model.rt60_mid
-        rt60_high = self.model.rt60_high
+        rt60_low = np.atleast_1d(self.model.rt60_low)
+        rt60_mid = np.atleast_1d(self.model.rt60_mid)
+        rt60_high = np.atleast_1d(self.model.rt60_high)
 
-        self.view.display_rt60_values(rt60_low, rt60_mid, rt60_high)
+
+
+        min_len = min(rt60_low.size, rt60_mid.size, rt60_high.size)
+
+        rt60_low = rt60_low[:min_len]
+        rt60_mid = rt60_mid[:min_len]
+        rt60_high = rt60_high[:min_len]
+
+        self.model.set_rt60_values(rt60_low, rt60_mid, rt60_high)
+
+
+
+        # Convert to numpy arrays before passing to view
+        self.view.display_rt60_values(np.array(rt60_low), np.array(rt60_mid), np.array(rt60_high),
+                                      self.merge_rt60_values, self.unmerge_rt60_values)
+
+
+    def merge_rt60_values(self, rt60_low, rt60_mid, rt60_high):
+        # Create a new figure for the merged graph
+        plt.figure(figsize=(10, 4))
+
+        # Plot the merged graph
+        plt.plot(rt60_low, label='Low Frequency', color='blue')
+        plt.plot(rt60_mid, label='Mid Frequency', color='green')
+        plt.plot(rt60_high, label='High Frequency', color='red')
+
+        # Set title and labels for the merged graph
+        plt.title('Merged RT60 for Low, Mid, High Frequencies')
+        plt.xlabel('Time (seconds)')
+        plt.ylabel('Power (dB)')
+        plt.legend()
+
+        # Show the merged plot
+        plt.show()
+
+
+    def compute_highest_resonance(self, audio_data, sample_rate):
+        # Compute the Fast Fourier Transform (FFT)
+        fft_result = np.fft.fft(audio_data)
+
+        # Calculate the frequencies corresponding to the FFT result
+        frequencies = np.fft.fftfreq(len(fft_result), d=1 / sample_rate)
+
+        # Find the index of the maximum amplitude (excluding DC component)
+        max_amplitude_index = np.argmax(np.abs(fft_result[1:])) + 1
+
+        # Return the frequency corresponding to the maximum amplitude
+        return frequencies[max_amplitude_index]
+
+    def unmerge_rt60_values(self, rt60_low, rt60_mid, rt60_high):
+        # Call the view's display_rt60_values method
+        self.view.display_rt60_values(rt60_low, rt60_mid, rt60_high, self.merge_rt60_values, self.unmerge_rt60_values)
+
+    def get_audio_duration(self):
+        # Check if audio_data is available in the model
+        if self.model.audio_data is not None:
+            # Convert audio_data to Pydub AudioSegment
+            audio_segment = AudioSegment(
+                data=self.model.audio_data.tobytes(),
+                sample_width=self.model.audio_data.dtype.itemsize,
+                frame_rate=self.model.sample_rate,
+                channels=1  # Assuming mono audio
+            )
+            return audio_segment.duration_seconds
+        else:
+            return 0  # Return 0 if audio_data is not available
+
 
 if __name__ == "__main__":
     root = tk.Tk()
--- view.py
+++ view.py
@@ -1,9 +1,9 @@
 # view.py
 import tkinter as tk
-from tkinter import filedialog
-import os
+import numpy as np
 import matplotlib.pyplot as plt
 import librosa.util
+
 
 class AudioView:
     def __init__(self, root, controller):
@@ -19,6 +19,10 @@ class AudioView:
         self.plot_button = tk.Button(root, text="Plot RT60", command=self.controller.plot_rt60)
         self.plot_button.pack(pady=10)
 
+        # Create the Merge Button and set its command attribute to a dummy function initially
+        self.merge_button = tk.Button(root, text="Merge Graphs", command=lambda: None)
+        self.merge_button.pack(pady=10)
+
     def display_file_name(self, file_name):
         self.result_label.config(text=f"File: {file_name}")
 
@@ -26,21 +30,114 @@ class AudioView:
         # Convert audio_data to floating-point format
         audio_data_float = librosa.util.normalize(audio_data.astype(float))
 
+        # Plot the waveform with units on axes
+        time = np.arange(0, len(audio_data_float)) / sample_rate  # Time in seconds
         plt.figure(figsize=(10, 4))
-        plt.plot(audio_data_float)
+        plt.plot(time, audio_data_float)
         plt.title('Waveform')
+        plt.xlabel('Time (seconds)')
+        plt.ylabel('Amplitude')
+
+        # Compute the highest resonance frequency
+        highest_resonance_freq = self.controller.compute_highest_resonance(audio_data, sample_rate)
+
+        # Move the text below the graph
+        plt.subplots_adjust(bottom=0.2)
+
+        # Add text for highest resonance frequency
+        plt.text(0.5, 0, f'Highest Resonance: {highest_resonance_freq:.2f} Hz', transform=plt.gca().transAxes,
+                 color='black', fontsize=12, fontweight='bold', ha='center')
+
         plt.show()
 
-    def display_rt60_values(self, rt60_low, rt60_mid, rt60_high):
-        frequencies = ['Low', 'Mid', 'High']
+    def compute_highest_resonance(self, audio_data, sample_rate):
+        # Compute the Fast Fourier Transform (FFT)
+        fft_result = np.fft.fft(audio_data)
 
-        # Check if any of the values is None and replace it with a default value (0 in this case)
-        rt60_low = rt60_low if rt60_low is not None else 0
-        rt60_mid = rt60_mid if rt60_mid is not None else 0
-        rt60_high = rt60_high if rt60_high is not None else 0
+        # Calculate the frequencies corresponding to the FFT result
+        frequencies = np.fft.fftfreq(len(fft_result), d=1 / sample_rate)
 
-        plt.bar(frequencies, [rt60_low, rt60_mid, rt60_high], color=['blue', 'green', 'red'])
-        plt.title('RT60 for Low, Mid, High Frequencies')
-        plt.xlabel('Frequency Range')
-        plt.ylabel('RT60 (seconds)')
+        # Find the index of the maximum amplitude (excluding DC component)
+        max_amplitude_index = np.argmax(np.abs(fft_result[1:])) + 1
+
+        # Return the frequency corresponding to the maximum amplitude
+        return frequencies[max_amplitude_index]
+
+    def display_rt60_values(self, rt60_low, rt60_mid, rt60_high, merge_function, unmerge_function):
+        # Convert scalar values to arrays
+        rt60_low = np.atleast_1d(rt60_low)
+        rt60_mid = np.atleast_1d(rt60_mid)
+        rt60_high = np.atleast_1d(rt60_high)
+
+        print("RT60 Low array:", rt60_low)
+        print("RT60 Mid array:", rt60_mid)
+        print("RT60 High array:", rt60_high)
+
+        # Determine the minimum length among the arrays
+        min_len = min(len(rt60_low), len(rt60_mid), len(rt60_high))
+
+        # Get the duration of the audio file using sound_duration
+        audio_duration = self.controller.get_audio_duration()
+
+        # Create subplots for the three individual graphs
+        fig, axs = plt.subplots(3, 1, figsize=(10, 12))
+
+        # Create time array for x-axis starting from zero and ending at audio duration
+        time = np.linspace(0, audio_duration, min_len, endpoint=False)  # Use endpoint=False
+
+        # Plot individual graphs
+        axs[0].plot(time, rt60_low[:min_len], label='Low Frequency', color='blue')
+        axs[1].plot(time, rt60_mid[:min_len], label='Mid Frequency', color='green')
+        axs[2].plot(time, rt60_high[:min_len], label='High Frequency', color='red')
+
+        # Set y-axis limits to better visualize short RT60 values
+        axs[0].set_ylim(0, 0.1)  # Adjust the upper limit as needed
+        axs[1].set_ylim(0, 0.1)
+        axs[2].set_ylim(0, 0.1)
+
+        # Set titles and labels for individual graphs
+        axs[0].set_title('RT60 for Low Frequency')
+        axs[0].set_xlabel('Time (seconds)')
+        axs[0].set_ylabel('Power (dB)')
+        axs[0].legend()
+
+        axs[1].set_title('RT60 for Mid Frequency')
+        axs[1].set_xlabel('Time (seconds)')
+        axs[1].set_ylabel('Power (dB)')
+        axs[1].legend()
+
+        axs[2].set_title('RT60 for High Frequency')
+        axs[2].set_xlabel('Time (seconds)')
+        axs[2].set_ylabel('Power (dB)')
+        axs[2].legend()
+
+        # Adjust layout to prevent clipping of titles and labels
+        plt.tight_layout()
+
+        # Update the command attribute of the Merge Button
+        self.merge_button.config(command=lambda: merge_function(rt60_low, rt60_mid, rt60_high))
+
+        # Show the plots
+        plt.show()
+
+
+    def merge_rt60_values(self, rt60_low, rt60_mid, rt60_high):
+        # Create time array for x-axis
+        time = np.arange(0, len(rt60_low))
+
+        # Create a new figure for the merged graph
+        plt.figure(figsize=(10, 4))
+
+        # Plot the merged graph
+        plt.plot(time, rt60_low, label='Low Frequency', color='blue')
+        plt.plot(time, rt60_mid, label='Mid Frequency', color='green')
+        plt.plot(time, rt60_high, label='High Frequency', color='red')
+
+        # Set title and labels for the merged graph
+        plt.title('Merged RT60 for Low, Mid, High Frequencies')
+        plt.xlabel('Time (seconds)')
+        plt.ylabel('Power (dB)')
+        plt.legend()
+
+        # Show the merged plot
         plt.show()
